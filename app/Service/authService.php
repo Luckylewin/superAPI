e404f8c889a4d5d1fa88349c6338d597eda55562
liziying <876505905@qq.com>
2018-11-09 17:44:17 +0800

    修改token[C

--- app/Service/authService.php
+++ app/Service/authService.php
@@ -470,13 +470,14 @@ class authService extends common
 
     private function _generateAccessToken($MAC)
     {
+        $valid_time = 3600 * 6;
         $cache = $this->getRedis(Redis::$REDIS_DEVICE_STATUS);
 
         if (time() - strtotime($cache->hGet($MAC,'logintime')) < 30) {
-             return $cache->hGet($MAC,'token');
+            $expire_time = time() + $valid_time - 40;
+            return ['token' => $cache->hGet($MAC,'token'),'expire' => $expire_time];
         }
 
-        $valid_time = 3600 * 6;
         $expire_time = $valid_time + time();
         AES::setKEY(substr(md5($MAC . AES::$_KEY),16,32));
         $encrypt = AES::encrypt(mt_rand(111,999)."|".$this->clientIP.'|'.$MAC.'|'.$expire_time);
