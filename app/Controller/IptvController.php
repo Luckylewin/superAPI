ad6bdbe420f23b20bd73b380cc769646e4513a6c
liziying <876505905@qq.com>
2018-12-12 19:15:57 +0800

    修改auth/token 请求方式

--- app/Controller/IptvController.php
+++ app/Controller/IptvController.php
@@ -23,10 +23,10 @@ class IptvController extends BaseController
      */
     public function auth()
     {
-        $mac       = $this->request->post('mac');
-        $timestamp = $this->request->post('timestamp');
-        $signature = $this->request->post('signature');
-
+        $mac       = $this->request->post('mac')?:$this->request->get('mac');
+        $timestamp = $this->request->post('timestamp')?:$this->request->get('timestamp');
+        $signature = $this->request->post('signature')?:$this->request->get('signature');
+            
         $authService = new authService($this->request);
         $token = $authService->login($mac, $timestamp, $signature);
         if ($token['status'] === false) {
